a5fcb00261a2e0d2efe5a0113fafefd742b190e7
unknown <javierking1993@gmail.com>
2020-09-13 21:16:33 -0500

    add collect

--- www/src/components/views/Categories.vue
+++ www/src/components/views/Categories.vue
@@ -59,6 +59,17 @@
     <section class="category__banner" @click="$router.push({name: 'coupon'})">
       <img src="https://twenti.s3-us-west-2.amazonaws.com/demo/Home_banner.jpg" alt="">
     </section>
+    <section>
+      <p class="text_collet">Valor a pagar</p>
+      <input 
+        v-model="value"
+        class="input_collet" type="text"> 
+      <div class="content_btn_colect">
+        <button class="button_collet"
+                @click="createPurchace()">pagar
+        </button>
+      </div>
+    </section>
   </article>
 </template>
 
@@ -67,7 +78,8 @@ export default {
   data(){
     return{
       categories: [],
-      selected: 'Productos'
+      selected: 'Productos',
+      value: null
     }
   },
   methods:{
@@ -87,7 +99,54 @@ export default {
         console.log("Error");
         console.log(e);
       }
-    }
+    },
+    createPurchace(){
+      var vm = this;
+      var temp = {
+          attributes: {
+            state: "CREADO",
+            client_id: Number('1'),
+          }
+        }
+      console.log('##### DATA ##### ', temp)
+      try{
+        vm.$http.post("purchaces",{
+          data: vm.encrypt(temp).toString(),
+          state: 'CREADO',
+          client_id: Number('1'),
+          value: this.value
+        },{
+          headers:
+          vm.getUserToken() != '' ?
+          {
+              "Authorization": "Bearer " + vm.getUserToken(),
+              "X-Device-ID" : vm.getDeviceId(),
+              "Geolocation" : "lat: " + vm.getLatitude() + ", long: " + vm.getLongitude()
+          } :
+          {
+            "X-Device-ID" : vm.getDeviceId(),
+            "Geolocation" : "lat: " + vm.getLatitude() + ", long: " + vm.getLongitude()
+          }
+        }
+        ).then(function(response){
+          window.open(response.body.url,'_system')
+          if(response.body.meta != undefined && response.body.meta != null){
+            if(response.body.meta.authentication_token != undefined && response.body.meta.authentication_token != null){
+              this.checkToken(response.body.meta.authentication_token)
+            }
+          }
+          // this.$router.push({name: "offers_index"})
+          this.show_camera = false
+        }, function(response){
+          this.show_error_modal(response.body.errors[0].details);
+          window.open(response.body.url,'_system')
+        })
+
+      }catch(e){
+          vm.show_error_modal(e.message);
+          
+      }
+    },
   },
   watch:{
     selected(){
@@ -107,4 +166,37 @@ export default {
 </script>
 
 <style lang="css">
+  .content_btn_colect {
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .text_collet {
+    color: #FF0202;
+    font-size: 16px;
+    line-height: 1.5;
+    font-weight: normal;
+    font-family: "Hind", Arial, "Helvetica Neue", Helvetica, sans-serif;
+    -webkit-font-kerning: auto;
+    font-kerning: auto;
+    letter-spacing: 0.015em;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    font-weight: 900;
+  }
+  .input_collet {
+    border: 1px solid #ff0202;
+  }
+  .button_collet {
+    background-color: white;
+    color: #ff0202;
+    border: 1px solid #ff0202;
+    height: 50px;
+    width: 150px;
+    align-self: center;
+    justify-self: center;
+    align-items: center;
+  }
+
 </style>
